bbe5f1d8a8f75781e5aa658e0e33cceb33b193fd
Luiz SSB <luizssb.biz@gmail.com>
2018-04-24 22:28:07 -0300

    Ajuste do storyboard e novas células de projeto e "carregando". Na célula de projeto, foi considerado seriamente usar uma view customizada para mostrar os dados do usuário com o tamanho ideal, mas a ideia foi jogada fora em prol de manter a listagem sem flutuação.

---
+++ NewWayChallenge/View/ProjectTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ProjectTableViewCell.swift
+//  NewWayChallenge
+//
+//  Created by Luiz SSB on 4/24/18.
+//  Copyright © 2018 luizssb. All rights reserved.
+//
+
+import UIKit
+
+class ProjectTableViewCell: UITableViewCell {
+    @IBOutlet weak var repositoryLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var forksLabel: UILabel!
+    @IBOutlet weak var starsLabel: UILabel!
+    @IBOutlet weak var ownerImageView: UIImageView!
+    @IBOutlet weak var ownerUsernameLabel: UILabel!
+    @IBOutlet weak var ownerRealNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
